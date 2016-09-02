99357024e1ea8e18202d0a62513d4cb7e7b7263e
Leon <leonp1991@gmail.com>
2016-09-02 16:03:53 +1000

    folder resolution broken

--- source/components/index.jsx
+++ source/components/index.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import * as actions from '../logic/user/actions';
 
-import Login from './login';
+import Login from './login_temp';
 import Home from './home';
 
 import './styles.scss';
